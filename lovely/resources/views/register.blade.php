907ca99b539d0496744a8cd58f60946e9b17c5bc
tonyooo <talaogonanthony07@gmail.com>
2026-07-26 14:01:48 +0800

    added some bugs

--- lovely/resources/views/register.blade.php
+++ lovely/resources/views/register.blade.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <h1>Registration Form</h1>
-    <label for="">Firstname</label>
+    <label for="firstname">Firstname</label>
+    <input type="text" id="firstname" name="firstname">
 </body>
 </html>
